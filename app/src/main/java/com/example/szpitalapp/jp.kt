ff8e5ce43b1ced3b73154c24a06fc84629e394b5
MTGonda <mtg00747@gmail.com>
2021-05-11 02:27:17 +0200

    test

---
+++ app/src/main/java/com/example/szpitalapp/jp.kt
@@ -0,0 +1,11 @@
+package com.example.szpitalapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class jp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jp)
+    }
+}
